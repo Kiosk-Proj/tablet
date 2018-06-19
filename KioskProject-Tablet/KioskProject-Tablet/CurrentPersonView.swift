29ba7e6181a85951331a0b2ff64385d2b4d7e989
Alden <aldenblamp@gmail.com>
2018-06-19 16:36:49 -0400

    creating project

---
+++ KioskProject-Tablet/KioskProject-Tablet/CurrentPersonView.swift
@@ -0,0 +1,37 @@
+//
+//  CurrentPersonView.swift
+//  KioskProject-Tablet
+//
+//  Created by alden lamp on 6/19/18.
+//  Copyright © 2018 alden lamp. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CurrentPersonView: UIViewController{
+    
+    var nameLabel = UILabel()
+    var IDLable = UILabel()
+    var userImageView = UIImageView()
+    var dateLabel = UILabel()
+    var timeLabel = UILabel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+    func createLabels() -> UILabel{
+        let label = UILabel()
+        
+        
+        
+        
+    }
+    
+    
+    
+}
---
+++ KioskProject-Tablet/KioskProject-Tablet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KioskProject-Tablet
+//
+//  Created by alden lamp on 6/19/18.
+//  Copyright © 2018 alden lamp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
